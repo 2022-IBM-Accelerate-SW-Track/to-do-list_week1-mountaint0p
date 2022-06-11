dc725c919d552f263b4b6f8b1decffc64d6b568a
mountaint0p <summit100@gmail.com>
2022-06-10 23:56:20 -0400

    first changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
 export default class About extends Component {
-  render() {
-    return (
-      <div>
-        <p>Design your About me page </p> 
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<div>
+					<div class="split left">
+						<div className="centered">
+							<img
+								className="profile_image"
+								// Image goes here
+								alt="Profile Pic"
+							></img>
+						</div>
+					</div>
+					<div className="split right">
+						<div className="centered">
+							<div className="name_title">Summit Pradhan</div>
+							<div className="brief_description">
+								I'm from Morrisville, North Carolina, and I'm a computer science
+								major at Swarthmore College located in Swarthmore, Pennsylvania.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
